d7ee7c31d15949e3aae0e812a9a6b1fdaf1f91f7
Filippo Ranza <filipporanza@gmail.com>
2022-01-18 16:04:35 +0100

    Export s.t.g. factory and config

--- src/traffic_generator/mod.rs
+++ src/traffic_generator/mod.rs
@@ -5,6 +5,8 @@ mod traffic_generator;
 pub use simple_traffic_generator::SimpleTrafficGenerator;
 pub use traffic_generator::TrafficGenerator;
 
+pub use simple_traffic_generator_factory::{simple_traffic_generator_factory, SimpleTrafficGeneratorConfig};
+
 /// Allow to change probability type in no-time
 type Node = f64;
 /// Allow to change step type in no-time
